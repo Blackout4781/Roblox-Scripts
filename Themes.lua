88652e4a79a5e3bb181c6814e61326282fa34365
blackout <74207478+Blackout4781@users.noreply.github.com>
2023-06-24 00:43:05 -0500

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
   ThemeManager.BuiltInThemes = {
-		['Main'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"fd07ce","AccentColor":"780dff","BackgroundColor":"141414","OutlineColor":"323232"}') },
+		['Main'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"fd0707","AccentColor":"780dff","BackgroundColor":"141414","OutlineColor":"323232"}') },
 		['Dracula'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232533","AccentColor":"6271a5","BackgroundColor":"1b1c27","OutlineColor":"7c82a7"}') },
 		['Bitch Bot'] 		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality'] 		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
