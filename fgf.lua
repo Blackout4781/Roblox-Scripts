a80fd9bc67c7ca3d372c2c1f55d7a35e1946b7d1
blackout <74207478+Blackout4781@users.noreply.github.com>
2024-02-24 23:19:34 -0600

    Update fgf.lua

--- fgf.lua
+++ fgf.lua
@@ -1,4 +1,13 @@
+local function getServerConstant(value)
+    return value
+end
+
 local stringPattern = getServerConstant('%s(.)');
-return function (text)
-    return string.lower(text):gsub(stringPattern, string.upper);
-end;
+return function(text)
+    if not text or not stringPattern then
+        -- Handle nil values
+        return nil
+    end
+
+    return string.lower(text):gsub(stringPattern, string.upper)
+end
